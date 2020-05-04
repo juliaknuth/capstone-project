3ca7c7aa058c2315d1114cd62388a77d55158ac5
Julia Knuth <jk@Julias-MBP.fritz.box>
2020-05-04 14:50:32 +0200

    Add style to stats, no logic still

--- client/src/Stats.js
+++ client/src/Stats.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import placeholder from './images/Placeholder.png'
+import star from './images/star.svg'
+import back from './images/left-chevron.svg'
 
 export default function Stats() {
   return (
     <ContentWrapper>
-      <h2>Stats</h2>
+      <div className="navigate">
+        <img className="navigate__back" src={back} alt="go back"></img>
+        <h2>Stats</h2>
+        <img className="navigate__bookmark" src={star} alt="bookmark"></img>
+      </div>
+
       <section>
-        <img src={placeholder} alt="placeholder"></img>
+        <img className="placeholder" src={placeholder} alt="placeholder"></img>
         <div className="description">
           <h3>Title:</h3>
           <p>Placeholder</p>
           <h3>Platform:</h3>
+          <p>Placeholder</p>
+        </div>
+        <div className="info">
+          <h3>Genre:</h3>
+          <p>Placeholder</p>
+          <h3>Mode:</h3>
           <p>Placeholder</p>
         </div>
       </section>
@@ -26,29 +39,64 @@ const ContentWrapper = styled.main`
   margin-right: 8px;
   background: #f5f9f9;
 
+  .navigate {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    widht: auto;
+  }
+
   h2 {
-    margin-top: 36px;
-    margin-bottom: 32px;
-    margin-left: 32px;
     font-weight: 250;
+    font-size: auto;
+    margin-top: 24px;
+    align-self: center;
+    justify-content: center;
+    margin-left: 36px;
+  }
+
+  .navigate__back {
+    margin-top: 20px;
+    margin-left: 16px;
+    height: 32px;
+    filter: invert(30%);
+  }
+
+  .navigate__bookmark {
+    margin-top: 20px;
+    height: 32px;
+    justify-content: right;
+    margin-left: 58px;
+    filter: invert(30%);
   }
 
   .description {
     margin-top: 16px;
     margin-left: -60px;
   }
+
+  .info {
+    margin-top: 8px;
+    margin-left: 12px;
+  }
   h3 {
     font-weight: 220;
     height: 20px;
+    margin-left: 16px;
+  }
+
+  p {
+    font-weight: 200;
+    margin-left: 16px;
   }
 
   section {
+    margin-top: 16px;
     display: grid;
     grid-template-columns: auto auto;
-    grid-template-rows: 1 fr;
+    grid-template-rows: 2fr auto auto;
   }
 
-  img {
+  .placeholder {
     height: 200px;
     width: auto;
     margin-left: 20px;
