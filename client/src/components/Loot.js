0243f8c82455c1f28be7b9b69b9d6958b8e92ac8
Julia Knuth <jk@Julias-MBP.fritz.box>
2020-05-08 10:48:28 +0200

    Correct styling issues

--- client/src/components/Loot.js
+++ client/src/components/Loot.js
@@ -57,33 +57,4 @@ const ContentWrapper = styled.main`
   a:visited {
     color: black;
   }
-  li {
-    display: flex;
-    justify-content: space-between;
-    list-style: none;
-    margin-bottom: 20px;
-    margin-left: -8px;
-    font-weight: 150;
-    text-overflow: ellipsis;
-  }
-
-  button {
-    justify-content: flex-end;
-    margin-left: auto;
-    margin-right: 16px;
-    background: rgb(131, 131, 131);
-    background: radial-gradient(
-      circle,
-      rgba(131, 131, 131, 1) 1%,
-      rgba(125, 125, 125, 1) 100%
-    );
-    border-radius: 20%;
-    border: none;
-    color: white;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 12pt;
-    box-shadow: 1px 1px 1px rgba(68, 68, 68, 0.6);
-  }
 `
