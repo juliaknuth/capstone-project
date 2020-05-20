c46a3995e321505a745a89e88f7e1acf9a196464
Julia Knuth <jk@Julias-MBP.fritz.box>
2020-05-20 16:42:27 +0200

    Styled component for upload component fix

--- client/src/components/ImageUploader.js
+++ client/src/components/ImageUploader.js
@@ -1,30 +1,38 @@
 import React from 'react'
 import ImageUploader from 'react-images-upload'
+import styled from 'styled-components/macro'
 
 export default function Uploader({ setFormData, formData }) {
   return (
-    <ImageUploader
-      withIcon={false}
-      onChange={(m, f) => setFormData({ ...formData, image: f[0] })}
-      maxFileSize={5242880}
-      singleImage={true}
-      imgExtension={['.jpg', '.gif', '.png', '.gif']}
-      maxFileSize={5242880}
-      accept="image/*"
-      label=""
-      buttonText="upload image"
-      buttonStyles={{
-        background: '#fd474b',
-        'border-radius': '5px',
-        'font-size': '12pt',
-        'font-family': 'Rajdhani',
-      }}
-      fileContainerStyle={{
-        background: 'transparent',
-        height: '60px',
-        'box-shadow': 'none',
-        'margin-top': '-30px',
-      }}
-    />
+    <ContentWrapper>
+      <ImageUploader
+        withIcon={false}
+        onChange={(m, f) => setFormData({ ...formData, image: f[0] })}
+        maxFileSize={5242880}
+        singleImage={true}
+        imgExtension={['.jpg', '.gif', '.png', '.gif']}
+        maxFileSize={5242880}
+        accept="image/*"
+        label=""
+        buttonText="upload image"
+        fileContainerStyle={{}}
+      />
+    </ContentWrapper>
   )
 }
+const ContentWrapper = styled.div`
+  .fileContainer {
+    margin-top: -20px;
+    background: transparent;
+    height: 60px;
+    box-shadow: none;
+    margin-top: '-30px';
+  }
+
+  .chooseFileButton {
+    background: #fd474b;
+    border-radius: 5px;
+    font-size: 12pt;
+    font-family: 'Rajdhani', sans-serif;
+  }
+`
